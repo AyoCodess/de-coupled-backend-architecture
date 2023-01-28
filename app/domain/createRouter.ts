1d290f5af6f6079ade66e42ce67c26dd8c09845d
ayocodess <ayo.daniel.adesanya@gmail.com>
2023-01-28 16:55:28 +0100

    updated tsconfig

--- app/domain/createRouter.ts
+++ app/domain/createRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createIdentityApi } from "../../identity";
+import { createIdentityApi } from "./identity";
 
 export function createRouter(app: Router) {
   return {
