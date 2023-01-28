63251059c74e8d227f1436d2aac008fa30d7f241
ayocodess <ayo.daniel.adesanya@gmail.com>
2023-01-28 15:14:06 +0100

    added gitignore

--- build/identity/api/sign_in.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createSignIn = void 0;
-function createSignIn() {
-    return () => "signing in user...";
-}
-exports.createSignIn = createSignIn;
--- build/identity/api/verify.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createVerify = void 0;
-function createVerify() {
-    return () => "verifying user...";
-}
-exports.createVerify = createVerify;
