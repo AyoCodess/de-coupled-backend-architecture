dd2bba2f87ae5f3253a71fc16e6ad72cef62d828
ayocodess <ayo.daniel.adesanya@gmail.com>
2023-01-28 23:13:30 +0100

    updated pkg.json

--- src/domains/createRouter.ts
+++ src/domains/createRouter.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
+import { getStatus } from "../status/repo";
 import { createIdentityApi } from "./identity";
 import { createProductsApi } from "./products";
 
-export function createRouter(router: Router) {
+export function createRouter(app: Router) {
+  const router = Router();
+  app.use("/api", router);
   return {
     identity: createIdentityApi(router),
     products: createProductsApi(router),
+    status: getStatus(router),
   };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,15 @@
 import cors from "cors";
 import express from "express";
-import { Router } from "express";
 import { createRouter } from "./domains/createRouter";
 import { getServerStatus } from "./status";
 
 const port = 3000;
 
 const app = express();
-const router = Router();
 app.use(express.json());
 app.use(cors());
-app.use("/api", router);
 
-createRouter(router);
-getServerStatus(router);
+createRouter(app);
 
 app.listen(port, () => {
   console.log(`server is listening on port ${port}`);
